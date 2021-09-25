f922d6605705a19f4e400f5466b590c776da81f1
Michelle Husbands <mrhc93@gmail.com>
2021-09-25 15:30:34 +1000

    Fix repo

--- build/CMakeFiles/monitor.dir/src/processor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/monitor.dir/src/processor.cpp.o: \
- /media/sf_GitHub/Ubuntu_System_Monitor/src/processor.cpp \
- /usr/include/stdc-predef.h \
- /media/sf_GitHub/Ubuntu_System_Monitor/include/processor.h
